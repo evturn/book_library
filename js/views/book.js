42ae6da16a32a4d3e1ad86e7a4d2f9218ce0f444
Evan Turner <evturn@gmail.com>
2014-12-18 22:44:02 -0500

    book view created

--- js/views/book.js
+++ js/views/book.js
@@ -1,1 +1,14 @@
 var app = app || {};
+
+app.BookView = Backbone.View.extend({
+
+	tagName: 'div',
+	className: 'bookContainer',
+	template: _.template($('#bookTemplate').html()),
+
+	render: function(){
+		this.$el.html(this.template.(this.model.attributes));
+	return this;
+	}
+
+});
