52642922f3fa08a752b7bf48e307b38294c57644
Evan Turner <evturn@gmail.com>
2014-12-18 23:04:29 -0500

    junk data added to the document ready in order test the views

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = app || {};
 
 $(function(){
+    var books = [
+        {title: 'Shit Man Thats My Sandwich', author: 'Doug Funny', releaseDate: '2001', keywords: 'Anger and Dispair' },
+        { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', releaseDate: '2008', keywords: 'JavaScript Programming' },
+        { title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw', releaseDate: '2012', keywords: 'CoffeeScript Programming' },
+        { title: 'Scala for the Impatient', author: 'Cay S. Horstmann', releaseDate: '2012', keywords: 'Scala Programming' },
+        { title: 'American Psycho', author: 'Bret Easton Ellis', releaseDate: '1991', keywords: 'Novel Splatter' }
+    ];
 
-})
+    new.app.LibraryView(books);
+});
