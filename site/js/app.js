b7d9d2b833d918084227a06e7ece366a7092e99f
Evan Turner <evturn@gmail.com>
2014-12-22 18:50:26 -0500

    sample data removed from the document ready now that the fetch function will pull from the database

--- site/js/app.js
+++ site/js/app.js
@@ -1,13 +1,6 @@
 var app = app || {};
 
 $(function(){
-    var books = [
-        {title: 'Shit Man Thats My Sandwich', author: 'Doug Funny', releaseDate: '2001', keywords: 'Anger and Dispair' },
-        { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', releaseDate: '2008', keywords: 'JavaScript Programming' },
-        { title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw', releaseDate: '2012', keywords: 'CoffeeScript Programming' },
-        { title: 'Scala for the Impatient', author: 'Cay S. Horstmann', releaseDate: '2012', keywords: 'Scala Programming' },
-        { title: 'American Psycho', author: 'Bret Easton Ellis', releaseDate: '1991', keywords: 'Novel Splatter' }
-    ];
 
-    new app.LibraryView(books);
+    new app.LibraryView();
 });
