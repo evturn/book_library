3d89124aea8cc815a375fcac441a4d9f71fbb747
Evan Turner <evturn@gmail.com>
2014-12-22 19:09:36 -0500

    parse function for id added to book model

--- site/js/models/book.js
+++ site/js/models/book.js
@@ -1,6 +1,10 @@
 var app = app || {};
 
 app.Book = Backbone.Model.extend({
+    parse: function(response){
+    	response.id = response._id
+    	return response;
+    },
     defaults: {
         coverImage: 'img/placeholder.png',
         title: 'No title',
